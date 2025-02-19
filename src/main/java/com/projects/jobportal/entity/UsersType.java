21532246072628a0b4d581e0cd47602742f9254c
Shivam Patel <shivampatelcodes01@gmail.com>
2025-02-19 18:00:37 -0500

    Defined users and usersType JPA entities

---
+++ src/main/java/com/projects/jobportal/entity/UsersType.java
@@ -0,0 +1,58 @@
+package com.projects.jobportal.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users_type")
+public class UsersType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String userTypeId;
+
+    private String userTypeName;
+
+    @OneToMany(targetEntity = Users.class, mappedBy = "usersTypeId", cascade = CascadeType.ALL)
+    private List<Users> users;
+
+    public UsersType() {}
+
+    public UsersType(String userTypeId, String userTypeName) {
+        this.userTypeId = userTypeId;
+        this.userTypeName = userTypeName;
+    }
+
+    public String getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(String userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
+    public List<Users> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Users> users) {
+        this.users = users;
+    }
+
+    @Override
+    public String toString() {
+        return "UsersType{" +
+                "userTypeId='" + userTypeId + '\'' +
+                ", userTypeName='" + userTypeName + '\'' +
+                '}';
+    }
+}
