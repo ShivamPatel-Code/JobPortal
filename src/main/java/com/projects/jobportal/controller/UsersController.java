8bb9f02a4cf4c493e1683151174d4c0a97e8d60e
Shivam Patel <shivampatelcodes01@gmail.com>
2025-02-20 00:17:40 -0500

    add try catch to register api

--- src/main/java/com/projects/jobportal/controller/UsersController.java
+++ src/main/java/com/projects/jobportal/controller/UsersController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class UsersController {
@@ -23,11 +24,17 @@ public class UsersController {
     }
 
     @GetMapping("/register")
-    public String register(Model model){
-        List<UsersType> usersTypes =  usersTypeService.getAll();
-        model.addAttribute("getAllTypes", usersTypes);
-        model.addAttribute("user", new Users());
-        return "register";
+    public String register(Model model) {
+        try {
+            List<UsersType> usersTypes = usersTypeService.getAll();
+            model.addAttribute("getAllTypes", usersTypes);
+            model.addAttribute("user", new Users());
+            return "register";
+        } catch (Exception e) {
+            // Log the exception using a logger instead of printing the stack trace
+            LoggerFactory.getLogger(UsersController.class).error("Error occurred while fetching user types: {}", e.getMessage(), e);
+            return "error";
+        }
     }
 
     @PostMapping("/register/new")
